82b20d5196d43ecf5495a91a344131c564547a85
kristianvv <kristianvaag@gmail.com>
2022-11-14 15:08:52 +0100

    Oppdatert HWR

---
+++ index.js
@@ -0,0 +1,38 @@
+var today = new Date();
+var date = ((today.getFullYear)(today.getMonth+1));
+let personer = [];
+// Eksempel
+const addPerson = (ev)=>{
+    ev.preventDefault();  //Stopper html form fra å bekrefte
+    let person = {
+        trid: "################",
+        tid: today,
+        navn: document.getElementById('navn').value,
+        email: document.getElementById('email').value,
+        alder: document.getElementById('alder').value,
+        søvn: document.getElementById('mon').value,
+        ankomst: document.getElementById('sos').value,
+        sosialtype: document.getElementById('tre').value,
+        bestjobbeplass: document.getElementById('søv').value,
+        kommunikasjon: document.getElementById('kom').value,
+        tidsfrist: document.getElementById('meu').value,
+        løsningsorientasjon: document.getElementById('seh').value
+    }
+    personer.push(person);
+    document.querySelector('form').reset(); //Fjerner neste entries
+
+    //Logger visning av JSON-objekt
+    console.warn('added' , {personer} );
+    let pre = document.querySelector('#msg pre');
+    pre.textContent = '' + JSON.stringify(personer, '/t', 2);
+
+    //lagrer til localStorage
+    localStorage.setItem('Personer', JSON.stringify(personer) );
+}
+document.addEventListener('DOMContentLoaded', ()=>{ 
+    document.getElementById('btn').addEventListener('click', addPerson);
+
+    var id = Math.random() * 16 | 0, v = v == '#' ? v : (v & 0x3 | 0x8);
+    result = v.toString(16);
+    return '################'.replace(/[#]/).toString.trid
+});
