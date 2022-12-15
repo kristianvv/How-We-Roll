d98f143c27f1e9a4e3ef297a55a00be61fc5580d
kristianvv <kristianvaag@gmail.com>
2022-12-15 16:55:17 +0100

    La til .json link etter spørreskjema

--- index.js
+++ index.js
@@ -3,24 +3,15 @@ let personer = [];
 const addPerson = (ev)=>{
     ev.preventDefault();  //Stopper html form fra å bekrefte
     let person = {
-        trid: id,
-        tid: str,
-        navn: document.getElementById('navn').value,
-        email: document.getElementById('email').value,
-        alder: document.getElementById('alder').value,
-        søvn: document.getElementById('mon').value,
-        ankomst: document.getElementById('sos').value,
-        sosialtype: document.getElementById('tre').value,
-        bestjobbeplass: document.getElementById('søv').value,
-        kommunikasjon: document.getElementById('kom').value,
-        tidsfrist: document.getElementById('meu').value,
-        løsningsorientasjon: document.getElementById('seh').value
+        trid: trxid,
+        tid: time,
     }
     personer.push(person);
     document.querySelector('form').reset(); //Fjerner neste entries
 
     //Logger visning av JSON-objekt
     console.warn('added');
-    let pre = document.querySelector('#msg pre');
+    let time = document.querySelector('#msg pre');
+    let trxid = document.querySelector('#msg pre');
     pre.textContent = '' + JSON.stringify(personer, '/t', 2);
 }
